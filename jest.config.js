9a5192ebce2ce357b7541f5633d2064eaa28b273
GustaVasconcelos <gustavo.vasconcelos.peixoto18@gmail.com>
2023-10-21 22:46:32 -0300

    feat: adicionando testes unitarios para controller create

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+const config = {
+    testEnvironment: 'node',
+    testMatch: ['**/__tests__/**/*.js?(x)', '**/?(*.)+(spec|test).js?(x)']
+};
+
+
+export default config;
---
+++ src/test/user/create.test.js
@@ -0,0 +1,208 @@
+import create from '../../controllers/userController/create.js';
+import { createService, findEmailService, findCpfService } from '../../services/user.services.js';
+import validator from "validator";
+import { cpf as cpfValidator } from 'cpf-cnpj-validator';
+
+// Mockando o módulo de serviços
+jest.mock('../../services/user.services.js');
+
+describe("Create User Tests", () => {
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
+    test("Should return an error when there are empty fields", async () => {
+
+        const request = {
+            body: {
+                name: "", 
+                email: "test@example.com",
+                cpf: "123.456.789-01",
+                password: "test123",
+                confirmPassword: "test123"
+            }
+        }
+    
+        const response = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn()
+        }
+    
+        await create(request, response);
+    
+        expect(findEmailService).not.toHaveBeenCalled();
+        expect(findCpfService).not.toHaveBeenCalled();
+        expect(createService).not.toHaveBeenCalled();
+        expect(response.status).toHaveBeenCalledWith(400);
+        expect(response.json).toHaveBeenCalledWith({ "error": "Há campos vazios!" });
+    
+    });
+
+    test("should check if the email is valid", () => {
+
+        const validEmail = "lucia@teste.com";
+    
+        const isValidEmail = validator.isEmail(validEmail);
+    
+        expect(isValidEmail).toBe(true);
+    
+    });
+
+    test("should check if the email is not valid", () => {
+
+        const validEmail = "invalid-email";
+    
+        const isValidEmail = validator.isEmail(validEmail);
+    
+        expect(isValidEmail).toBe(false);
+    
+    });
+
+    test("must return an error when email already exists in the database", async () => {
+        const existingEmail = "existing@teste.com";
+    
+        const request = {
+            body: {
+                name: "Lucia",
+                email: existingEmail,
+                cpf: "901.338.624-53",
+                password: "test1",
+                confirmPassword: "test1"
+            }
+        }
+    
+        const response = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn()
+        }
+
+        findEmailService.mockResolvedValueOnce(true);
+    
+        await create(request, response);
+    
+        expect(findEmailService).toHaveBeenCalledWith(existingEmail);
+        expect(response.status).toHaveBeenCalledWith(400);
+        expect(response.json).toHaveBeenCalledWith({ "error": "Email já cadastrado!" });
+    });
+
+    test("must return an error when cpf already exists in the database", async () => {
+        const existingCpf = "901.338.624-53";
+    
+        const request = {
+            body: {
+                name: "Lucia",
+                email: "lucia@teste.com",
+                cpf: existingCpf,
+                password: "test1",
+                confirmPassword: "test1"
+            }
+        }
+    
+        const response = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn()
+        }
+
+        findCpfService.mockResolvedValueOnce(true);
+    
+        await create(request, response);
+    
+        expect(findCpfService).toHaveBeenCalledWith(existingCpf);
+        expect(response.status).toHaveBeenCalledWith(400);
+        expect(response.json).toHaveBeenCalledWith({ "error": "CPF já cadastrado!" });
+    });
+
+    test("should check if the cpf is valid", () => {
+
+        const validCpf = "901.338.624-53";
+    
+        const isValidCpf = cpfValidator.isValid(validCpf);
+    
+        expect(isValidCpf).toBe(true);
+    
+    });
+
+    test("should return an error if passwords are different", () => {
+
+        const validCpf = "901.338.624-53";
+    
+        const isValidCpf = cpfValidator.isValid(validCpf);
+    
+        expect(isValidCpf).toBe(true);
+    
+    });
+
+    test("must return an error when passwords do not match", async () => {
+        const request = {
+            body: {
+                name: "Lucia",
+                email: "lucia@teste.com",
+                cpf: "901.338.624-53",
+                password: "test1", 
+                confirmPassword: "test2" 
+            }
+        }
+    
+        const response = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn()
+        }
+    
+        await create(request, response);
+    
+        expect(createService).not.toHaveBeenCalled(); 
+        expect(response.status).toHaveBeenCalledWith(400);
+        expect(response.json).toHaveBeenCalledWith({ "error": "As senhas são diferentes!" });
+    });
+
+    test("must register the user in the database successfully", async () => {
+        const request = {
+            body: {
+                name: "Lucia",
+                email: "lucia@teste.com",
+                cpf: "901.338.624-53",
+                password: "teste",
+                confirmPassword: "teste"
+            }
+        }
+
+        const response = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn()
+        }
+        
+        
+        await create(request, response);
+
+        expect(createService).toHaveBeenCalledTimes(1);
+        expect(createService).toHaveBeenCalledWith(request.body);
+        expect(response.status).toHaveBeenCalledWith(201);
+    })
+
+    test("It should return an error when not registering the user", async () => {
+
+        const request = {
+            body: {
+                name: "Lucia",
+                email: "lucia@teste.com",
+                cpf: "901.338.624-53",
+                password: "teste",
+                confirmPassword: "teste"
+            }
+        }
+
+        const response = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn()
+        }
+
+        createService.mockRejectedValue(new Error("Mocking exception"))
+
+        await create(request, response);
+
+        expect(createService).toHaveBeenCalledTimes(1);
+        expect(response.status).toHaveBeenCalledWith(500);
+    })
+
+});
