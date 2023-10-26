51415e0d3a20630bafed7fac14ed0c54803528e2
GustaVasconcelos <gustavo.vasconcelos.peixoto18@gmail.com>
2023-10-26 20:59:56 -0300

    feat:adicionando documentação no services

--- src/services/itemTrainingSheet.services.js
+++ src/services/itemTrainingSheet.services.js
@@ -1,11 +1,51 @@
 import itemTrainingSheet from '../models/ItemTrainingSheet.js';
 
+/**
+ * Encontra um item de ficha de treinamento pelo ID.
+ * @param {string} id - ID do item de ficha de treinamento.
+ * @returns {Promise<ItemTrainingSheet>} - O item de ficha de treinamento encontrado.
+ */
 const findItemTrainingSheetIdService = (id) => itemTrainingSheet.findById(id);
+
+/**
+ * Busca todos os itens de ficha de treinamento.
+ * @returns {Promise<ItemTrainingSheet[]>} - Um array de itens de ficha de treinamento.
+ */
 const findService = () => itemTrainingSheet.find();
+
+/**
+ * Cria um novo item de ficha de treinamento com os dados fornecidos.
+ * @param {object} body - Dados do item de ficha de treinamento a serem criados.
+ * @returns {Promise<ItemTrainingSheet>} - O item de ficha de treinamento criado.
+ */
 const createService = (body) => itemTrainingSheet.create(body);
+
+/**
+ * Deleta um item de ficha de treinamento pelo ID.
+ * @param {string} id - ID do item de ficha de treinamento a ser deletado.
+ * @returns {Promise<ItemTrainingSheet>} - O item de ficha de treinamento deletado.
+ */
 const deleteService = (id) => itemTrainingSheet.findByIdAndDelete(id);
-const findAllItemsTrainingSheetService = (trainingSheetId) => itemTrainingSheet.find({trainingSheetId});
-const updateService = (id, name, series, repetitions, member) => itemTrainingSheet.findByIdAndUpdate(id, {name, series, repetitions, member}, {new:true});
+
+/**
+ * Busca todos os itens de ficha de treinamento relacionados a uma ficha de treinamento específica.
+ * @param {string} trainingSheetId - ID da ficha de treinamento associada aos itens.
+ * @returns {Promise<ItemTrainingSheet[]>} - Um array de itens de ficha de treinamento relacionados à ficha de treinamento.
+ */
+const findAllItemsTrainingSheetService = (trainingSheetId) => itemTrainingSheet.find({ trainingSheetId });
+
+/**
+ * Atualiza os dados de um item de ficha de treinamento.
+ * @param {string} id - ID do item de ficha de treinamento a ser atualizado.
+ * @param {string} name - Novo nome do item.
+ * @param {string} series - Novo número de séries do item.
+ * @param {string} repetitions - Novo número de repetições do item.
+ * @param {string} member - Novo membro associado ao item.
+ * @returns {Promise<ItemTrainingSheet>} - O item de ficha de treinamento atualizado.
+ */
+const updateService = (id, name, series, repetitions, member) => {
+    return itemTrainingSheet.findByIdAndUpdate(id, { name, series, repetitions, member }, { new: true });
+};
 
 export {
     createService,
@@ -14,4 +54,4 @@ export {
     findAllItemsTrainingSheetService,
     updateService,
     findItemTrainingSheetIdService
-}
+};
--- src/services/trainingSheet.services.js
+++ src/services/trainingSheet.services.js
@@ -1,10 +1,48 @@
 import trainingSheet from '../models/TrainingSheet.js';
 
-const findService = () => trainingSheet.find();
-const findTrainingSheetIdService = (id) => trainingSheet.findById(id);
-const findTrainingSheetUserIdService = (user_id) => trainingSheet.find({user_id});
+/**
+ * Cria uma nova ficha de treinamento com os dados fornecidos.
+ * @param {object} body - Dados da ficha de treinamento a serem criados.
+ * @returns {Promise<TrainingSheet>} - A ficha de treinamento criada.
+ */
 const createService = (body) => trainingSheet.create(body);
-const updateService = (id, name, updated_at) => trainingSheet.findByIdAndUpdate(id, {name, updated_at}, {new:true});
+
+/**
+ * Busca todas as fichas de treinamento.
+ * @returns {Promise<TrainingSheet[]>} - Um array de fichas de treinamento.
+ */
+const findService = () => trainingSheet.find();
+
+/**
+ * Encontra uma ficha de treinamento pelo ID.
+ * @param {string} id - ID da ficha de treinamento.
+ * @returns {Promise<TrainingSheet>} - A ficha de treinamento encontrada.
+ */
+const findTrainingSheetIdService = (id) => trainingSheet.findById(id);
+
+/**
+ * Encontra fichas de treinamento pelo ID do usuário.
+ * @param {string} user_id - ID do usuário associado às fichas de treinamento.
+ * @returns {Promise<TrainingSheet[]>} - Um array de fichas de treinamento encontradas para o usuário.
+ */
+const findTrainingSheetUserIdService = (user_id) => trainingSheet.find({ user_id });
+
+/**
+ * Atualiza os dados de uma ficha de treinamento.
+ * @param {string} id - ID da ficha de treinamento a ser atualizada.
+ * @param {string} name - Novo nome da ficha de treinamento.
+ * @param {string} updated_at - Nova data de atualização.
+ * @returns {Promise<TrainingSheet>} - A ficha de treinamento atualizada.
+ */
+const updateService = (id, name, updated_at) => {
+  return trainingSheet.findByIdAndUpdate(id, { name, updated_at }, { new: true });
+};
+
+/**
+ * Deleta uma ficha de treinamento pelo ID.
+ * @param {string} id - ID da ficha de treinamento a ser deletada.
+ * @returns {Promise<TrainingSheet>} - A ficha de treinamento deletada.
+ */
 const deleteService = (id) => trainingSheet.findByIdAndDelete(id);
 
 export {
@@ -13,5 +51,5 @@ export {
     findTrainingSheetIdService,
     findTrainingSheetUserIdService,
     updateService,
-    deleteService,
-}
+    deleteService
+};
